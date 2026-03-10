99fa4bb1cbe03d71742019d8da705943c9b5f3ad
Simon <simon.julia89@gmail.com>
2026-03-10 09:08:06 +0100

    feat: digital garden introduction

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -7,7 +7,7 @@ export default {
   },
   digitalGarden: {
     definition: "A digital garden is a non-linear collection of evolving notes, ideas, and resources. It is different from a blog in a way that is less organized and can change over time like a garden.",
-    introduction: ""
+    introduction: "I'm passionate about always learning something, and I have thought of making notes for my future self or for other developers who might find my summaries useful."
   },
   footer: {
     copyright: "- © Júlia Simon 2021 -"
--- src/i18n/hu.ts
+++ src/i18n/hu.ts
@@ -7,7 +7,7 @@ export default {
   },
   digitalGarden: {
     definition: "A digitális kert folyamatosan fejlődő jegyzetek, ötletek és források nemlineáris gyűjteménye. Abban különbözik a blogoktól, hogy kevésbé szervezett, és idővel változhat, akár egy kert.",
-    introduction: ""
+    introduction: "Nagyon szeretek új dolgokat tanulni, és arra gondoltam, hogy készítek néhány jegyzetet a tanultakról jövőbeli énemnek vagy más olyan fejlesztőknek, akik ezeket érdekesnek találhatják."
   },
   footer: {
     copyright: "- © Simon Júlia 2021 -"
