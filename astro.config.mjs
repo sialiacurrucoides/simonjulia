272e2834c85be05ceb6de419f5b53d0a18a829ae
Simon <simon.julia89@gmail.com>
2026-03-18 12:58:14 +0100

    feat: add openDyslexic support and use astro fonts api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [
+        {
+            provider: fontProviders.fontsource(),
+            name: "Montserrat",
+            cssVariable: "--font-normal",
+            weights: [400, 700],
+        },
+        {
+            provider: fontProviders.fontsource(),
+            name: "OpenDyslexic",
+            cssVariable: "--font-dyslexic",
+            weights:[400, 700]
+        }
+],
+});
