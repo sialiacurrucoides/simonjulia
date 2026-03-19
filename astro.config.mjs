2f9b79712f2a0c4d207bfd8296f2af3c1816b842
Simon <simon.julia89@gmail.com>
2026-03-19 14:11:22 +0100

    feat: add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,24 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    fonts: [
-        {
-            provider: fontProviders.fontsource(),
-            name: "Montserrat",
-            cssVariable: "--font-normal",
-            weights: [400, 700],
-        },
-        {
-            provider: fontProviders.fontsource(),
-            name: "OpenDyslexic",
-            cssVariable: "--font-dyslexic",
-            weights:[400, 700]
-        }
+  fonts: [
+      {
+          provider: fontProviders.fontsource(),
+          name: "Montserrat",
+          cssVariable: "--font-normal",
+          weights: [400, 700],
+      },
+      {
+          provider: fontProviders.fontsource(),
+          name: "OpenDyslexic",
+          cssVariable: "--font-dyslexic",
+          weights:[400, 700]
+      }
 ],
+  site: 'https://simonjulia.hu/',
+  integrations: [sitemap()]
 });
